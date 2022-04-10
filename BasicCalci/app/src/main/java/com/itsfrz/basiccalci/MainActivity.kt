b7cc75a609b6cfe5b31a35e99e520aada76e23a7
ItsFRZ <faraz.ar.sheikh@gmail.com>
2022-04-10 17:36:13 +0530

    Calculator without 3rd Party Library

--- BasicCalci/app/src/main/java/com/itsfrz/basiccalci/MainActivity.kt
+++ BasicCalci/app/src/main/java/com/itsfrz/basiccalci/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.Exception
 import java.util.*
+import kotlin.collections.HashMap
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,184 +17,156 @@ class MainActivity : AppCompatActivity() {
     private var output : TextView? = null;
     private var rawData : TextView? = null;
 
-    private var history : Stack<String> = Stack();
+
+    private val history : Stack<String> = Stack();
+
+    private var oldData : String = ""
+    private var operand : String = "+"
+    private var isNewOp : Boolean = true
 
 
-    private var data : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        initAllViews()
+    }
+
+    private fun initAllViews() {
         input = findViewById(R.id.input);
         output = findViewById(R.id.output);
         rawData = findViewById(R.id.rawData);
-
     }
 
-    fun setData(input : String){
-        data += input
-    }
+    fun numberEvent(view: View) {
+        if(isNewOp)
+            input?.text = ""
+        isNewOp = false
+        var data = input?.text.toString().trim()
 
-    fun setView(){
-        output?.text = data;
-    }
+        when(view.id){
+            R.id.zero -> {
+                data += "0"
+            }
+            R.id.one -> {
+                data += "1"
+            }
+            R.id.two -> {
+                data += "2"
+            }
+            R.id.three -> {
+                data += "3"
+            }
+            R.id.four -> {
+                data += "4"
+            }
+            R.id.five -> {
+                data += "5"
+            }
+            R.id.six -> {
+                data += "6"
+            }
+            R.id.seven -> {
+                data += "7"
+            }
+            R.id.eight -> {
+                data += "8"
+            }
+            R.id.nine -> {
+                data += "9"
+            }
+            R.id.left_paranthesis -> {
+                data += "("
+            }
+            R.id.right_paranthesis -> {
+                data += ")"
+            }
+            R.id.dot -> {
+                data += "."
+            }
 
-    fun setView(data : String){
-        this.data = data
-        setView()
-    }
-
-    fun One(view: View) {
-         setData("1")
-         setView()
-         clearRawData()
-    }
-    fun Two(view: View) {
-        setData("2")
-        setView()
-        clearRawData()
-    }
-    fun Three(view: View) {
-        setData("3")
-        setView()
-        clearRawData()
-    }
-    fun Four(view: View) {
-        setData("4")
-        setView()
-        clearRawData()
-    }
-    fun Five(view: View) {
-        setData("5")
-        setView()
-        clearRawData()
-    }
-    fun Six(view: View) {
-        setData("6")
-        setView()
-        clearRawData()
-    }
-    fun Seven(view: View) {
-        setData("7")
-        setView()
-        clearRawData()
-    }
-    fun Eight(view: View) {
-        setData("8")
-        setView()
-        clearRawData()
-    }
-    fun Nine(view: View) {
-        setData("9")
-        setView()
-        clearRawData()
-    }
-
-    fun leftParanthesis(view: View) {
-        setData("(")
-        setView()
-        clearRawData()
-    }
-    fun rightParanthesis(view: View) {
-        setData(")")
-        setView()
-        clearRawData()
-    }
-
-    fun modulo(view: View) {
-        setData("%")
-        setView()
-        clearRawData()
-    }
-    fun dot(view: View) {
-        setData(".")
-        setView()
-        clearRawData()
-    }
-    fun clearData(view: View) {
-        data = ""
-        setData(data)
-        setView()
-        clearRawData()
-    }
-
-    fun backPress(view: View) {
-        if(data.length >= 1){
-            val myData = data.substring(0,data.length-1);
-            clearData(view)
-            setData(myData)
-            setView()
-            clearRawData()
         }
+        input?.text = data ?: "";
+
+
     }
 
-    fun setRawData(myData  : String){
-        rawData?.text = myData ?: ""
-    }
+    fun operatorEvent(view: View) {
+        isNewOp = true;
+        oldData = input?.text.toString() ?: ""
 
-    fun clearRawData(){
-        rawData?.text = ""
+        when(view.id){
+            R.id.plus -> operand = "+"
+            R.id.multiply -> operand = "*"
+            R.id.divide -> operand = "/"
+            R.id.modulo -> operand = "%"
+            R.id.minus -> operand = "-"
+
+        }
     }
 
     fun equal(view: View) {
+        val newNumber : String = input?.text.toString() ?: ""
+        var result : Double = 0.0
+
         try{
-            val myData  = output?.text.toString() ?: ""
-            val result : Double = process(myData)
-            history.push(myData)
-            clearData(view);
-            setOutputResult(result);
-            setRawData(myData)
+            when(operand){
+                "+" -> {
+                    result = oldData.toDouble() + newNumber.toDouble()
+                }
+                "-" -> {
+                    result = oldData.toDouble() - newNumber.toDouble()
+                }
+                "/" -> {
+                    result = oldData.toDouble() / newNumber.toDouble()
+                }
+                "*" -> {
+                    result = oldData.toDouble() * newNumber.toDouble()
+                }
+                "%" -> {
+                    result = oldData.toDouble() % newNumber.toDouble()
+                }
+            }
 
         }catch (e : Exception){
             Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show()
+            clearData(view)
         }
+
+        output?.text = result.toString()
+        input?.text = result.toString()
+
+        setRawData(oldData,operand,newNumber)
+
+        val historyOfEntry = oldData+operand+newNumber
+        oldData = result.toString()
+
+        history.push(historyOfEntry+"="+oldData)
+
     }
 
-    private fun setOutputResult(result: Double) {
-        val temp = result.toString()
-        setView(temp)
+    private fun setRawData(oldData: String, operand: String, newNumber: String) {
+        val rawString = oldData+operand+newNumber
+
+        rawData?.text = rawString
     }
 
-    private fun process(expression: String): Double {
-       return Evaluate().eval(expression);
-    }
-
-    fun multiply(view: View) {
-        setData("*")
-        setView()
-        clearRawData()
-    }
-    fun divide(view: View) {
-        setData("/")
-        setView()
-        clearRawData()
-    }
-    fun plus(view: View) {
-        setData("+")
-        setView()
-        clearRawData()
-    }
-    fun minus(view: View) {
-        setData("-")
-        setView()
-        clearRawData()
+    fun clearData(view: View) {
+        input?.text = ""
+        output?.text = ""
+        rawData?.text = ""
+        oldData = ""
     }
 
     fun history(view: View) {
-        if(!history.isEmpty())
-        {
-            val undo = history.pop();
-            clearData(view)
-            setData(undo)
-            setView()
-            clearRawData()
-        }
+      if(history.size >= 1){
+          val(entry,result) = history.pop().split("=");
+          output?.text = result
+          rawData?.text = entry
+      }
     }
 
-    fun zero(view: View) {
-        setData("0")
-        setView()
-        clearRawData()
-    }
+
 }
